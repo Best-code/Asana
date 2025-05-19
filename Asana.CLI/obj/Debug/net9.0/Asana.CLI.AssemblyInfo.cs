6df0bec59d6d4dec7ef2868ba206034c1a17c6bc
Best-code <cpmraygun@gmail.com>
2025-05-19 18:06:00 -0400

    VSCode Trash

--- Asana.CLI/obj/Debug/net9.0/Asana.CLI.AssemblyInfo.cs
+++ Asana.CLI/obj/Debug/net9.0/Asana.CLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d0cffdd245c3a26aea8be1577a06118b86405fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fabbd689c50978113fb5038cd25f80f84fb57998")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
