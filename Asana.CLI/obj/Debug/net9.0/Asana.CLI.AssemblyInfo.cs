304d12145d0c757fab5a6716d6a292ba5d130a2b
Best-code <cpmraygun@gmail.com>
2025-05-19 12:46:48 -0400

    Added the functinoality to update and delete exist ing toDos. ToDos add, delete and update functinoality is inside of the project class. This completes #3

--- Asana.CLI/obj/Debug/net9.0/Asana.CLI.AssemblyInfo.cs
+++ Asana.CLI/obj/Debug/net9.0/Asana.CLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd42dc805bd7681573474cf007bbb8aaa996900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62232fa2b97b403bc79be0c7a8f77ff3013ba14a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
