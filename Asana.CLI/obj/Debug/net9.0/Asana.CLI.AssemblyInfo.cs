3d7dcc48bd1e55cbfaff089d6d545e5bba4f2d4b
Best-code <cpmraygun@gmail.com>
2025-05-19 10:20:57 -0400

    Init Commit

--- Asana.CLI/obj/Debug/net9.0/Asana.CLI.AssemblyInfo.cs
+++ Asana.CLI/obj/Debug/net9.0/Asana.CLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+10e2a0f0755d722c695f69b171d94233cc2fd412")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
