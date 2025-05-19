34db427cf1dd458f8b8bed9c3feee90c300c5a64
Best-code <cpmraygun@gmail.com>
2025-05-19 10:47:09 -0400

    Adds the ability to safely create and add a ToDo to the list. Completes #1

--- Asana.CLI/Program.cs
+++ Asana.CLI/Program.cs
@@ -7,7 +7,63 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<ToDo> ToDos = new List<ToDo>();
+
+            int selection = 0;
+            do
+            {
+                PrintOptions();
+                selection = HandleSelection(selection, ToDos);
+            } while (selection != 2);
+            
         }
+
+        static void PrintOptions()
+        {
+            Console.WriteLine($"1. Create ToDo");
+            Console.WriteLine($"2. Exit");
+        }
+
+        static int HandleInput()
+        {
+            var choice = Console.ReadLine() ?? "2";
+            int choiceInt;
+            int.TryParse(choice, out choiceInt);
+
+            return choiceInt;
+        }
+
+        static int HandleSelection(int selection, List<ToDo> ToDos)
+        {
+            selection = HandleInput();
+            switch (selection)
+            {
+                case 1:
+                    CreateTodo(ToDos);
+                    break;
+                case 2:
+                    break;
+                default:
+                    Console.WriteLine($"You did not enter a valid selection");
+                    break;
+            }
+
+            return selection;
+        }
+
+        static void CreateTodo(List<ToDo> ToDos)
+        {
+            ToDo createMe = new ToDo();
+            Console.WriteLine($"Name: ");
+            createMe.Name = Console.ReadLine();
+            Console.WriteLine($"Description: ");
+            createMe.Description = Console.ReadLine();
+
+            ToDos.Add(createMe);            
+
+            Console.WriteLine($"{createMe}");
+            
+        }
+
     }
 }
