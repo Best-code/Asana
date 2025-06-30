2454576fcfe2d83be334183afc1a5fe257c9cc8c
Best-code <cpmraygun@gmail.com>
2025-06-29 22:57:37 -0400

    Its back...

--- Asana.CLI/obj/Debug/net9.0/Asana.CLI.AssemblyInfo.cs
+++ Asana.CLI/obj/Debug/net9.0/Asana.CLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88111344b51cafeaec530220f33a0f8ef7508834")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64159b6b582aadcf9e96cb84282a1d680a9471c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
