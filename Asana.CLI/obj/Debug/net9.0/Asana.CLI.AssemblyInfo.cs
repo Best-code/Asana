370be903947250532f7227978a8f4df922a2fa97
Best-code <cpmraygun@gmail.com>
2025-07-22 11:14:14 -0400

    Services are now Locked - Completes #57

--- Asana.CLI/obj/Debug/net9.0/Asana.CLI.AssemblyInfo.cs
+++ Asana.CLI/obj/Debug/net9.0/Asana.CLI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f7fb6d81169eb58fa510f2f1a41229a93bb15b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49108dce0ef2651855e5621873d524ca79f26bb4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asana.CLI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
